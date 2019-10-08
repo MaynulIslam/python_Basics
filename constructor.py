662199c8896ea18f683f3c208af28e863565b912
MaynulIslam <maynulislam3@gmail.com>
2019-10-09 03:36:28 +0600

    one spelling mistake

--- constructor.py
+++ constructor.py
@@ -2,11 +2,11 @@ class testClass:
     def __init__(self):
         print ("I am a constructor")
 
-    def funtion1(self):
+    def function1(self):
         print("I am funtion one")
 
     def function2(self):
         print("I am function two")
 
 ob = testClass()
-ob.funtion1()
+ob.function2()
